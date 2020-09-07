e1040010d226c044170c934e5d2d2b3854e8eba3
KeatRed <keatnns@gmail.com>
2020-09-07 17:55:32 +0200

    debut du home

--- src/components/home.js
+++ src/components/home.js
@@ -2,7 +2,39 @@ import * as React from "react";
 
 class Home extends React.Component {
     render() {
-        return <h1>Bonjour, {this.props.name}</h1>;
+        return (
+            
+            <body>
+            <div class="testbox">
+              <form action="/">
+                <div class="banner">
+                  <h1>Enregistrer son vol</h1>
+                </div>
+                <p>Runner Information</p>
+                <div class="item">
+                  <label for="nom">Nom<span>*</span></label>
+                  <input id="nom" type="text" name="nom" required/>
+                </div>
+                <div class="item">
+                  <label for="prenom">Prenom<span>*</span></label>
+                  <input id="prenom" type="text" name="prenom" required/>
+                </div>
+                <div class="item">
+                  <p>Vol</p>
+                  <select>
+                    <option selected value="" disabled selected></option>
+                    <option value="vol1" >DTW-JFK(300)</option>
+                    <option value="vol1">CDG-JFK(1000)</option>
+                    <option value="vol1">CDG-DTW(700)</option>
+                  </select>
+                </div>
+                <div class="btn-block">
+                  <button type="submit" href="/">SUBMIT</button>
+                </div>
+              </form>
+            </div>
+          </body>);
+        
     }
 }
 
